7a9ce10b857f94d8b9899c78dafbab4aa742ae53
allbecauseyoutoldmeso <kate.gleeson@thoughtbot.com>
2019-11-14 18:17:25 +0000

    not pretty but it works

--- src/Board.js
+++ src/Board.js
@@ -2,6 +2,65 @@ import React from 'react';
 import Square from './Square'
 import Crocodile from "./Crocodile"
 
+class GridMaker {
+  constructor(dimension) {
+    this.dimension = dimension
+  }
+
+  coordinates() {
+    var array = []
+
+    for(var x = 0; x < this.dimension; x++) {
+      for(var y = 0; y < this.dimension; y++) {
+        array.push({
+          x: x,
+          y: y,
+        })
+      }
+    }
+
+    return array
+  }
+
+  addCrocodile(obj) {
+    obj['crocodile'] = (Math.random() < 0.3)
+  }
+
+  addNumber(obj, coordinates) {
+
+  }
+
+  newGrid() {
+    var coordinates = this.coordinates(3)
+
+    coordinates.forEach(this.addCrocodile)
+
+    coordinates.forEach(function (a) {
+
+      var neighbours = coordinates.filter(function(b) {
+        if(a === b) {
+          return false
+        } else if ([b['x'] - 1, b['x'], b['x'] + 1].includes(a['x']) || [b['y'] - 1, b['y'], b['y'] + 1].includes(a['y'])) {
+          return true
+        } else {
+          return false
+        }
+      })
+
+      var crocodiles = neighbours.filter(function(c) {
+        return c['crocodile'] === true
+      })
+
+      a['number'] = crocodiles.length
+    })
+
+    return coordinates
+  }
+}
+
+
+
+
 function content() {
   if (Math.random() > 0.2) {
     return '?'
@@ -10,40 +69,20 @@ function content() {
   }
 }
 
-function coordinates(num) {
-  var array = Array()
-
-  for(var x = 0; x < num; x++) {
-    for(var y = 0; y < num; y++) {
-      array.push({
-        x: x,
-        y: y,
-      })
-    }
-  }
-
-  return array
-}
-
-function addCrocodile(obj) {
-  obj['crocodiles'] = (Math.random() < 0.3)
-}
-
-
-// var coordinates = coordinates(3)
-// coordinates.forEach(addCrocodile)
-// coordinates.forEach(function (obj) {
-//   console.log(obj['x'])
-// })
-
 class Board extends React.Component {
   renderSquare() {
     return <Square content={content()}/>;
   }
 
+  grid() {
+    var gridMaker = new GridMaker(3)
+    return gridMaker.newGrid()
+  }
+
   render() {
     return (
       <div>
+        {  }
         <div className="board-row">
           {this.renderSquare()}
           {this.renderSquare()}

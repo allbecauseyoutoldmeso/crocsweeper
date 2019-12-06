9ad0f1291b448e4b0825d35c23c8ffde81b11e7e
allbecauseyoutoldmeso <kate.gleeson@thoughtbot.com>
2019-12-06 14:39:27 +0000

    Updates

---
+++ precache-manifest.de67384b61d491c50bc96e791a43eb04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "205c7f6cc557f40e0953ce0eee2e4450",
+    "url": "/crocsweeper/index.html"
+  },
+  {
+    "revision": "1a52eab6ce364d28d56d",
+    "url": "/crocsweeper/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "4acaba34e5cb138bec90",
+    "url": "/crocsweeper/static/css/main.8f076aa0.chunk.css"
+  },
+  {
+    "revision": "1a52eab6ce364d28d56d",
+    "url": "/crocsweeper/static/js/2.133e9515.chunk.js"
+  },
+  {
+    "revision": "4acaba34e5cb138bec90",
+    "url": "/crocsweeper/static/js/main.318ca943.chunk.js"
+  },
+  {
+    "revision": "c89b48be993df339b872",
+    "url": "/crocsweeper/static/js/runtime-main.6a98a05c.js"
+  }
+]);
